30eddc53477d13228126275a37cf89ee23412e79
Olha Salatova <olga.sharpmind@gmail.com>
2023-03-22 22:45:57 +0100

    updated prettier settings: indent lines with tabs instead of spaces

--- src/components/expenseForm/ExpenseForm.tsx
+++ src/components/expenseForm/ExpenseForm.tsx
@@ -1,52 +1,52 @@
 import { useState } from 'react'
 import CategorySelector, {
-    DefinedCategory,
-    Category,
-    CATEGORY_LIST,
-    isDefinedCategory,
+	DefinedCategory,
+	Category,
+	CATEGORY_LIST,
+	isDefinedCategory,
 } from '../categorySelector/CategorySelector'
 import styles from './expenseForm.module.css'
 
 export default function ExpenseForm(props: {
-    onAddRecord: (category: DefinedCategory, amount: number) => void
+	onAddRecord: (category: DefinedCategory, amount: number) => void
 }) {
-    const [category, setCategory] = useState<Category>('DEFAULT')
-    const [amount, setAmount] = useState(0)
+	const [category, setCategory] = useState<Category>('DEFAULT')
+	const [amount, setAmount] = useState(0)
 
-    const minExpenseValue = 0.01
+	const minExpenseValue = 0.01
 
-    const handleSubmit = (e: React.MouseEvent) => {
-        e.preventDefault()
+	const handleSubmit = (e: React.MouseEvent) => {
+		e.preventDefault()
 
-        if (
-            !isDefinedCategory(category) ||
-            isNaN(amount) ||
-            amount < minExpenseValue
-        ) {
-            return
-        }
-        props.onAddRecord(category, amount)
-        setCategory('DEFAULT')
-        setAmount(0)
-    }
+		if (
+			!isDefinedCategory(category) ||
+			isNaN(amount) ||
+			amount < minExpenseValue
+		) {
+			return
+		}
+		props.onAddRecord(category, amount)
+		setCategory('DEFAULT')
+		setAmount(0)
+	}
 
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.header}>Add expenses</h2>
-            <CategorySelector
-                optionList={Object.values(CATEGORY_LIST) as Category[]}
-                selectedOption={category}
-                onSelect={setCategory}
-            />
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.header}>Add expenses</h2>
+			<CategorySelector
+				optionList={Object.values(CATEGORY_LIST) as Category[]}
+				selectedOption={category}
+				onSelect={setCategory}
+			/>
 
-            <input
-                type="number"
-                min={minExpenseValue}
-                value={amount}
-                placeholder="Spent amount"
-                onChange={(event) => setAmount(parseFloat(event.target.value))}
-            />
-            <button onClick={handleSubmit}>Save</button>
-        </div>
-    )
+			<input
+				type="number"
+				min={minExpenseValue}
+				value={amount}
+				placeholder="Spent amount"
+				onChange={(event) => setAmount(parseFloat(event.target.value))}
+			/>
+			<button onClick={handleSubmit}>Save</button>
+		</div>
+	)
 }
--- src/components/expenseRecord/ExpenseRecord.tsx
+++ src/components/expenseRecord/ExpenseRecord.tsx
@@ -2,17 +2,17 @@ import { DefinedCategory } from '../categorySelector/CategorySelector'
 import styles from './expenseRecord.module.css'
 
 export default function ExpenseRecord(props: {
-    index: number
-    categoryName: DefinedCategory
-    amount: number
+	index: number
+	categoryName: DefinedCategory
+	amount: number
 }) {
-    return (
-        <div className={styles.containerFlex}>
-            <div>
-                <span>{props.index + 1}. </span>
-                <span>{props.categoryName}</span>
-            </div>
-            <span className={styles.amount}>${props.amount}</span>
-        </div>
-    )
+	return (
+		<div className={styles.containerFlex}>
+			<div>
+				<span>{props.index + 1}. </span>
+				<span>{props.categoryName}</span>
+			</div>
+			<span className={styles.amount}>${props.amount}</span>
+		</div>
+	)
 }
