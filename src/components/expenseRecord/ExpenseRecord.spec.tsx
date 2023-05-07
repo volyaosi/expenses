66070574874475b8313fe443cc65c6d457608a93
Olha Salatova <olga.sharpmind@gmail.com>
2023-05-07 22:54:43 +0200

    test settings

--- src/components/expenseRecord/ExpenseRecord.spec.tsx
+++ src/components/expenseRecord/ExpenseRecord.spec.tsx
@@ -1,5 +1,13 @@
-describe('Mock test', () => {
+import { ExpenseRecord } from './ExpenseRecord'
+import { render, screen } from '@testing-library/react'
+
+describe('ExpenseRecord', () => {
     it('true shpuld be true', () => {
         expect(true).toBe(true)
+    })
+
+    it('should render', () => {
+        render(<ExpenseRecord category="Home" amount={1} index={1} />)
+        expect(screen.getByText('Home')).toBeTruthy()
     })
 })
