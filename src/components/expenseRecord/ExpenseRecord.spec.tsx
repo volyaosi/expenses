44ede4fd7db2d06cbc70ab1e7799c21adcaf2ca7
Olha Salatova <olga.sharpmind@gmail.com>
2023-05-11 19:20:26 +0200

    jest setup

--- src/components/expenseRecord/ExpenseRecord.spec.tsx
+++ src/components/expenseRecord/ExpenseRecord.spec.tsx
@@ -2,10 +2,6 @@ import { ExpenseRecord } from './ExpenseRecord'
 import { render, screen } from '@testing-library/react'
 
 describe('ExpenseRecord', () => {
-    it('true shpuld be true', () => {
-        expect(true).toBe(true)
-    })
-
     it('should render', () => {
         render(<ExpenseRecord category="Home" amount={1} index={1} />)
         expect(screen.getByText('Home')).toBeTruthy()
